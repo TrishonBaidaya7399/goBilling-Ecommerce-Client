0fbe1b27e3b9e02a478b9920a185611dbf9d30e0
TrishonBaidaya7399 <githubtrishon@gmail.com>
2024-02-28 21:16:53 +0600

    increase and decrease funtionality of quantity is added

--- src/Pages/Cart/CartItemRow.jsx
+++ src/Pages/Cart/CartItemRow.jsx
@@ -68,7 +68,7 @@ const CartItemRow = ({ item }) => {
       <div className="editIcon">
         <FiEdit className="text-xl"/>
       </div>
-      <div className="details grid grid-cols-4 rounded-sm w-full p-2 border-2 border-gray-500">
+      <div className="details grid grid-cols-4 items-center rounded-sm w-full p-2 border-2 border-gray-500">
         <div className="ProductName">{item?.name}</div>
         <div className="price">${item?.price}</div>
         <div className="quantity flex gap-1 items-center">
@@ -80,7 +80,7 @@ const CartItemRow = ({ item }) => {
             <MdAddCircle />
             </button>
         </div>
-        <div className="totalPrice flex gap-1">
+        <div className="totalPrice flex gap-1 ml-auto ">
           ${item?.quantity * item.price}
         </div>
       </div>
