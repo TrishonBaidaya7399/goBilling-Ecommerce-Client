efed4cb2cd7af1e22be8a75e150fdb7ddb507ea0
TrishonBaidaya7399 <githubtrishon@gmail.com>
2024-02-28 20:35:21 +0600

    Remove funtionality from the cart added

--- src/Pages/Cart/CartItemRow.jsx
+++ src/Pages/Cart/CartItemRow.jsx
@@ -1,9 +1,47 @@
+import axios from "axios";
 import PropTypes from "prop-types";
 import { FiEdit } from "react-icons/fi";
 import { MdAddCircle, MdRemoveCircle } from "react-icons/md";
 import { RiDeleteBinLine } from "react-icons/ri";
+import Swal from "sweetalert2";
 
 const CartItemRow = ({ item }) => {
+  // console.log(item);
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Want to remove?",
+      text: "You will not able to retrieve it!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#D1A054B2",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Delete",
+    })
+      .then((result) => {
+        if (result.isConfirmed) {
+          console.log("Delete itemId req: ", id);
+          axios.delete(`http://localhost:5000/cart/${id}`).then((res) => {
+            if (res.data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your item has been deleted.",
+                icon: "success",
+              });
+              setTimeout(() => {
+                window.location.reload();
+              }, 1000);
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          title: "Can not Delete!",
+          text: error.message,
+          icon: "error",
+        });
+      });
+  };
   return (
     <div className="flex items-center gap-1 text-gray-500 mb-[-2px]">
       <div className="editIcon">
@@ -21,9 +59,9 @@ const CartItemRow = ({ item }) => {
           ${item?.quantity * item.price}
         </div>
       </div>
-      <div className="editIcon">
+      <button className="editIcon" onClick={()=>handleDelete(item?._id)}>
       <RiDeleteBinLine className="text-red-500 text-xl"/>
-      </div>
+      </button>
     </div>
   );
 };
